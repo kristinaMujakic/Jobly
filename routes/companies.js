f8442966c8616e76a8809d242f8be677b0f2f56c
krissy <kmujakic@gmail.com>
2023-10-10 20:16:52 +0200

    adding validator for company search

--- routes/companies.js
+++ routes/companies.js
@@ -6,11 +6,12 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
+const { ensureIsAdmin } = require("../middleware/auth");
 const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companySearchSchema = require("../schemas/companySearch.json");
 
 const router = new express.Router();
 
@@ -21,7 +22,7 @@ const router = new express.Router();
  *
  * Returns { handle, name, description, numEmployees, logoUrl }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
 router.post("/", ensureIsAdmin, async function (req, res, next) {
@@ -47,13 +48,18 @@ router.post("/", ensureIsAdmin, async function (req, res, next) {
  * - maxEmployees
  * - nameLike (will find case-insensitive, partial matches)
  *
- * Authorization required: none
+ * Authorization required: admin
  */
 
 router.get("/", async function (req, res, next) {
   const { name, minEmployees, maxEmployees } = req.query;
 
   try {
+    const validator = jsonschema.validate(req.query, companySearchSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
     let companies;
 
     // Show all companies if no queries passed in
@@ -79,7 +85,7 @@ router.get("/", async function (req, res, next) {
  *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
  *   where jobs is [{ id, title, salary, equity }, ...]
  *
- * Authorization required: none
+ * Authorization required:admin
  */
 
 router.get("/:handle", async function (req, res, next) {
@@ -99,7 +105,7 @@ router.get("/:handle", async function (req, res, next) {
  *
  * Returns { handle, name, description, numEmployees, logo_url }
  *
- * Authorization required: login
+ * Authorization required: admin
  */
 
 router.patch("/:handle", ensureIsAdmin, async function (req, res, next) {
@@ -119,7 +125,7 @@ router.patch("/:handle", ensureIsAdmin, async function (req, res, next) {
 
 /** DELETE /[handle]  =>  { deleted: handle }
  *
- * Authorization: login
+ * Authorization: admin
  */
 
 router.delete("/:handle", ensureIsAdmin, async function (req, res, next) {
